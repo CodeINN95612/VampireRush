e33fc0e1a67225ffce05f39e0718ea34b21643f8
damian briones <damian.briones@outlook.com>
2024-02-17 13:30:21 -0500

    Camera Movement

--- scripts/my_camera.gd
+++ scripts/my_camera.gd
@@ -1,19 +1,27 @@
 extends Camera2D
 
-@export var min_speed: float = 0  # Speed of the camera movement
-@export var max_speed: float = 2  # Speed of the camera movement
+@export var speed: float = 2  # Speed of the camera movement
+@export var threshold_value: float = 100.0  # Speed of the camera movement
 var target := Vector2()  # Target position
 
+var move_zone_start: Vector2;
+
 func _ready():
-	target = position  # Initialize target to current camera position
+	var size = get_viewport_rect().size;
+	var end_x = size.x / zoom.x * 0.5 - threshold_value;
+	var end_y = size.y / zoom.y * 0.5 - threshold_value;
+	var start = Vector2(end_x, end_y)
+	
+	move_zone_start = start
 
 func _process(delta):
+	
 	var mouse_pos = get_global_mouse_position()  # Get the global mouse position
 	var screen_center = get_screen_center_position()  # Get the center of the screen
-	var distance = mouse_pos.distance_to(screen_center)
-	var local_speed = map(distance, 0, 300, min_speed, max_speed);
-	target = mouse_pos  # Set the target to the mouse position
-	position = position.lerp(target, local_speed * delta);
+	var relative_mouse_pos = mouse_pos-screen_center;
+	
+	if(abs(relative_mouse_pos.x) >= move_zone_start.x or abs(relative_mouse_pos.y) >= move_zone_start.y):
+		position = position.lerp(mouse_pos, speed * delta);
 	
 func map(value, istart, istop, ostart, ostop):
 	return ostart + (ostop - ostart) * ((value - istart) / (istop - istart))
